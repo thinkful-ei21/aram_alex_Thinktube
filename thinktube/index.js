8b63b5837f16600284a6a99a467058be716e602b
alex <alex@localhost.localdomain>
2018-05-15 09:39:53 -0700

    oh boy

--- thinktube/index.js
+++ thinktube/index.js
@@ -2,20 +2,34 @@
 
 /*global $ */
 
-function test(searchTerm, callback) {
-  const CLIENT_ID = '1094858048131-jng7t456tdnjduu1adso0ee9fe8fd18b.apps.googleusercontent.com';
-  const YOUTUBE = 'https://www.googleapis.com/youtube/v3/search';
-  const query = {
-    part: 'snippet',
-    key: CLIENT_ID,
-    q: `${searchTerm}`,
+// function test(searchTerm, callback) {
+//   const CLIENT_ID = '1094858048131-jng7t456tdnjduu1adso0ee9fe8fd18b.apps.googleusercontent.com';
+//   const YOUTUBE = 'https://www.googleapis.com/youtube/v3/search';
+//   const query = {
+//     part: 'snippet',
+//     key: CLIENT_ID,
+//     q: `${searchTerm}`
+//   };
+//   $.getJSON(YOUTUBE, query, callback);
+
+// }
+
+
+// test('test', (response) => {
+//   console.log(response);
+// });
+
+$.getJSON(
+  "https://www.googleapis.com/youtube/v3/search",
+  {
+    // part: 'snippet',
+    key: '1094858048131-jng7t456tdnjduu1adso0ee9fe8fd18b.apps.googleusercontent.com',
+    q: 'Rick Rolled',
     per_page: 5
-  };
-  $.getJSON(YOUTUBE, query, callback);
+  },
+  callbackFunction
+)
 
-}
-
-
-test('test', (response) => {
-  console.log(response);
-});
+function callbackFunction(data) {
+  console.log(data);
+} 
