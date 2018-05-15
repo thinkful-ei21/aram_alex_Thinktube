a12be5b43af99164a981de606817675589b7cd1f
Aram Hammoudeh <aramcontact@gmail.com>
2018-05-15 10:04:42 -0600

    Switching driver

--- thinktube/index.js
+++ thinktube/index.js
@@ -3,8 +3,11 @@
 /*global $ */
 
 function test(searchTerm, callback) {
+  const CLIENT_ID = '1094858048131-jng7t456tdnjduu1adso0ee9fe8fd18b.apps.googleusercontent.com';
   const YOUTUBE = 'https://www.googleapis.com/youtube/v3/search';
   const query = {
+    part: 'snippet',
+    key: CLIENT_ID,
     q: `${searchTerm}`,
     per_page: 5
   };
